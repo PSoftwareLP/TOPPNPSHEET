ff7534e080348331e94c8092b60fc1b2c472d015
Lukas Polten <Polten@bwl.tu-darmstadt.de>
2024-07-28 17:15:52 +0200

    Added the class logic - but you still need to adjust each input to get the correct attributes and the inspiration or xp to get the correct basi values

--- Version4/Logic.js
+++ Version4/Logic.js
@@ -23,13 +23,17 @@ new Vue({
         heroClasses: [
             { id: 0, name: "Held", minimumlevel: 0, Staerke: 0, Geschick: 0, Vitalitaet: 0, Konstitution: 0, Intelligenz: 0, Willskraft: 0, Charisma: 0, free: 5 },
             { id: 1, name: "Krieger", minimumlevel: 2, Staerke: 1, Geschick: 0, Vitalitaet: 1, Konstitution: 1, Intelligenz: 0, Willskraft: 1, Charisma: 0, free: 2 },
-            { id: 2, name: "Ranger", minimumlevel: 2, Staerke: 0, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 0, Willskraft: 1, Charisma: 0, free: 2 },
-            { id: 3, name: "Magier", minimumlevel: 2, Staerke: 1, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 2 },
+            { id: 2, name: "Scout", minimumlevel: 2, Staerke: 0, Geschick: 1, Vitalitaet: 0, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 0, free: 2 },
+            { id: 3, name: "Magier", minimumlevel: 2, Staerke: 0, Geschick: 0, Vitalitaet: 1, Konstitution: 0, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 2 },
             { id: 4, name: "Supporter", minimumlevel: 2, Staerke: 1, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 2 },
-            { id: 5, name: "Ritter ", minimumlevel: 8, Staerke: 1, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 5 },
-            { id: 6, name: "Waldlaufer", minimumlevel: 8, Staerke: 1, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 5 },
-            { id: 7, name: "Erzmagier", minimumlevel: 8, Staerke: 1, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 5 },
-            { id: 8, name: "Barde", minimumlevel: 8, Staerke: 1, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 5 },
+            { id: 5, name: "Ritter", minimumlevel: 8, Staerke: 2, Geschick: 0, Vitalitaet: 2, Konstitution: 2, Intelligenz: 0, Willskraft: 0, Charisma: 0, free: 2 },
+            { id: 6, name: "Gladiator", minimumlevel: 8, Staerke: 3, Geschick: 0, Vitalitaet: 1, Konstitution: 2, Intelligenz: 0, Willskraft: 0, Charisma: 0, free: 2 },
+            { id: 7, name: "Ranger", minimumlevel: 8, Staerke: 0, Geschick: 2, Vitalitaet: 0, Konstitution: 2, Intelligenz: 1, Willskraft: 1, Charisma: 0, free: 2 },
+            { id: 8, name: "Schattensoldat", minimumlevel: 8, Staerke: 0, Geschick: 3, Vitalitaet: 0, Konstitution: 1, Intelligenz: 0, Willskraft: 1, Charisma: 1, free: 2 },
+            { id: 9, name: "Erzmagier", minimumlevel: 8, Staerke: 0, Geschick: 1, Vitalitaet: 0, Konstitution: 1, Intelligenz: 3, Willskraft: 2, Charisma: 0, free: 2 },
+            { id: 10, name: "Hexenmeister", minimumlevel: 8, Staerke: 0, Geschick: 0, Vitalitaet: 2, Konstitution: 1, Intelligenz: 1, Willskraft: 2, Charisma: 0, free: 2 },
+            { id: 11, name: "Barde", minimumlevel: 8, Staerke: 0, Geschick: 1, Vitalitaet: 1, Konstitution: 1, Intelligenz: 1, Willskraft: 1, Charisma: 1, free: 2 },
+            { id: 12, name: "Priester", minimumlevel: 8, Staerke: 0, Geschick: 0, Vitalitaet: 0, Konstitution: 0, Intelligenz: 1, Willskraft: 3, Charisma: 2, free: 2 },
         ],
         attributes: [
             { name: "Staerke", inputValue: 0, istGeuebt: false, baseValue: 0, savingThrow: 0 },
@@ -90,21 +94,24 @@ new Vue({
         selectedShopItemId: null
     },
     methods: {
-        async fetchData(file, key) {
-            try {
-                const response = await fetch(`/${file}`);
-                const data = await response.json();
-                this[key] = data;
-            } catch (error) {
-                console.error(`Failed to load ${file}:`, error);
-            }
-        },
+
+
         compute() {
-            /*changes in the attribute trigger the updateAttribute function. Therefore this "this.computeAllAttributes();" is not necessary.*/
+            this.computeAllAttributes();
             this.computeAllBasicValues();
             this.computeAllAbilityModifiers();
             this.computeAllAttacks();
         },
+
+
+
+        /* HERO CLASS */
+        filteredHeroClasses() {
+            return this.heroClasses.filter(heroClass => heroClass.minimumlevel <= this.characterData.level);
+        },
+
+
+
         /*ATTRIBUTES*/
         computeAllAttributes() {
             for (let attribute in this.attributes) {
@@ -114,10 +121,15 @@ new Vue({
         updateAttribute(attribute) {
             attribute.baseValue = this.computeAttributeBaseValue(attribute);
             attribute.savingThrow = this.computeAttributeSavingThrow(attribute);
-            this.compute();
         },
         computeAttributeBaseValue(attribute) {
-            return Math.floor(attribute.inputValue / 10);
+            if (attribute.inputValue < 0) {
+                attribute.inputValue = 0;
+            }
+            let gewaehlteHeroClass = this.heroClasses.find(hClass => hClass.name === this.gewaehlteklasse);
+            let heroClassAttributeBonusValue = Math.abs(gewaehlteHeroClass[attribute.name]);
+
+            return Math.floor((Math.abs(attribute.inputValue) + (Math.abs(heroClassAttributeBonusValue) * Math.abs(this.characterData.level)))/10);
         },
         computeAttributeSavingThrow(attribute) {
             if (attribute.istGeuebt) {
@@ -141,8 +153,12 @@ new Vue({
             return usedPoints;
         },
         computeMaximumPoints() {
-            return 40 + this.characterData.level * 5;
+            let gewaehlteHeroClass = this.heroClasses.find(hClass => hClass.name === this.gewaehlteklasse);
+            return this.characterData.level * Math.abs(gewaehlteHeroClass["free"]);
         },
+
+
+
         /*BASIC VALUES*/
         computeAllBasicValues() {
             this.computeLevel();
@@ -155,7 +171,15 @@ new Vue({
             this.computeAusdauer();
         },
         computeLevel() {
-            this.characterData.level = Math.floor((3250 + Math.sqrt(Math.max(1150 * this.characterData.xp - 785000, 0))) / 1150);
+            let levelCompute = 1;
+            if (this.characterData.xp <= 300) {
+                levelCompute = 1;
+            }
+            else
+            {
+                levelCompute = Math.floor((3250 + Math.sqrt(Math.max(1150 * this.characterData.xp - 785000, 0))) / 1150);
+            }
+            this.characterData.level = levelCompute;
         },
         computeUebungsbonus() {
             this.characterData.Uebungsbonus = 1 + Math.floor(this.characterData.level / 3);
@@ -182,27 +206,39 @@ new Vue({
             this.characterData.Bewegung = 8 + this.getAttributeFromName("Geschick").baseValue + Math.floor(this.getAttributeFromName("Vitalitaet").baseValue / 2) + bonusBewegungTemp;
         },
         computeLeben() {
-            this.characterData.Leben = (10 + this.getAttributeFromName("Vitalitaet").inputValue * 3 + Math.floor(this.getAttributeFromName("Vitalitaet").baseValue * 4));
+            let gewaehlteHeroClass = this.heroClasses.find(hClass => hClass.name === this.gewaehlteklasse);
+            let heroClassAttributeBonusValue = Math.abs(gewaehlteHeroClass["Vitalitaet"]);
+            this.characterData.Leben = (10 + (Math.abs(this.getAttributeFromName("Vitalitaet").inputValue) + Math.abs(heroClassAttributeBonusValue) * Math.abs(this.characterData.level)) * 3 + Math.floor(this.getAttributeFromName("Vitalitaet").baseValue * 4));
         },
         computeMana() {
-            this.characterData.Mana = (10 + this.getAttributeFromName("Willskraft").inputValue * 3 + Math.floor(this.getAttributeFromName("Willskraft").baseValue * 4));
+            let gewaehlteHeroClass = this.heroClasses.find(hClass => hClass.name === this.gewaehlteklasse);
+            let heroClassAttributeBonusValue = Math.abs(gewaehlteHeroClass["Willskraft"]);
+            this.characterData.Mana = (10 + (Math.abs(this.getAttributeFromName("Willskraft").inputValue) + (Math.abs(heroClassAttributeBonusValue) * Math.abs(this.characterData.level))) * 3 + Math.abs(this.getAttributeFromName("Willskraft").baseValue * 4));
         },
         computeAusdauer() {
-            this.characterData.Ausdauer = (10 + this.getAttributeFromName("Konstitution").inputValue * 3 + this.getAttributeFromName("Konstitution").baseValue * 4);
+            let gewaehlteHeroClass = this.heroClasses.find(hClass => hClass.name === this.gewaehlteklasse);
+            let heroClassAttributeBonusValue = Math.abs(gewaehlteHeroClass["Konstitution"]);
+            this.characterData.Ausdauer = (10 + (Math.abs(this.getAttributeFromName("Konstitution").inputValue) + (Math.abs(heroClassAttributeBonusValue) * Math.abs(this.characterData.level))) * 3 + Math.abs(this.getAttributeFromName("Konstitution").baseValue) * 4);
         },
+
+
+
         /*ABILITLIES*/
         computeAllAbilityModifiers() {
             this.abilities.forEach(ability => {
-                this.computeAbilityModifiers(ability);
+                this.computeAbilityModifier(ability);
             });
         },
-        computeAbilityModifiers(ability) {
+        computeAbilityModifier(ability) {
             let attributeVal = this.getAttributeFromName(ability.baseAttribute).baseValue;
             if (ability.checked) {
                 attributeVal += this.characterData.Uebungsbonus;
             }
             ability.value = attributeVal;
         },
+
+
+
         /*ATTACKS*/
         computeAllAttacks() {
             this.attacks.forEach(attack => {
@@ -236,8 +272,11 @@ new Vue({
         removeAttack(attackId) {
             this.attacks = this.attacks.filter(attack => attack.id !== attackId);
         },
-        /*ITEMS*/
 
+
+
+
+        /*ITEMS*/
         addItemFromShop() {
             this.numberOfItems = this.numberOfItems + 1;
             const shopItem = this.itemsShop.find(item => item.id === this.selectedShopItemId);
@@ -251,10 +290,14 @@ new Vue({
         removeItem(itemId) {
             this.numberOfItems = this.numberOfItems - 1;
             this.items = this.items.filter(items => items.id !== itemId);
-            this.compute()
+            /*this.compute()*/
         },
-        /*ITEM SHOP*/
-        
+
+
+
+
+
+        /*ITEM SHOP*/        
         addItemToShop() {
             this.numberOfItemsInShop = this.numberOfItemsInShop + 1;
             newId = this.numberOfItemsInShop;
